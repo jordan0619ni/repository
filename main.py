041cdbd0acf23172a23d0b6356d5c3a63eb28fcd
Jordan <jordan0619.ni@gmail.com>
2025-09-15 15:44:58 +0800

    lesson1_1

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from repository!")
+#請建立一個Flask應用程式，並在根路由 ("/") 回傳 "Hello, World!" 的訊息。
+from flask import Flask
 
+app = Flask(__name__)
 
+@app.route("/")
+def hello():
+    return "Hello, World!"
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
