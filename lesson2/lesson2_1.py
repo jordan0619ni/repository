c9fa2c543d3ff762d1dca6f92665d5c6da480b22
Jordan <jordan0619.ni@gmail.com>
2025-09-22 12:18:37 +0800

    第二堂上午提交

--- lesson2/lesson2_1.py
+++ lesson2/lesson2_1.py
@@ -1,4 +1,13 @@
-if __name__ =="main":
-    print("這是python直接執行")
-else:
-    print("這不是python直接執行")
+def calculate_sum(a, b):
+    result = a + b
+    return result
+
+def main():
+    number1 = 10
+    number2 = 20
+    total = calculate_sum(number1, number2) #呼叫calculate_sum()
+    print(f"計算結果: {total}")
+
+
+if __name__ == "__main__":
+    main() # 呼叫main function
