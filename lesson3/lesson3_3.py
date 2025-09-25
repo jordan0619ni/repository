e1cf7ec67f8490606952625c7e73063680e48d55
Jordan <jordan0619.ni@gmail.com>
2025-09-25 14:34:22 +0800

    lesson3_3.py

--- lesson3/lesson3_3.py
+++ lesson3/lesson3_3.py
@@ -1,1 +1,18 @@
 import requests
+
+import requests
+
+url = 'https://data.ntpc.gov.tw/api/datasets/010e5b15-3823-4b20-b401-b1cf000550c5/json?page=0&size=1000'
+
+response = requests.get(url)
+#print(type(response))
+
+if response.status_code == 200:
+    print("下載成功")
+    print("下載的內容如下")
+    # print(response.text)
+    print(type(response.json()))
+    for item in response.json():
+        print(item)
+else:
+    print("下載失敗")
